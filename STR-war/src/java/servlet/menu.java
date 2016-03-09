d7f4f0d7d652532165e96158c42731abc4d8a8bc
5152683 <mariam.dalhoumi@outlook.com>
2016-03-09 11:29:23 +0100

    entité,facade,session(manque type_paiement et relation avec tarif)

--- STR-war/src/java/servlet/menu.java
+++ STR-war/src/java/servlet/menu.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package servlet;
 
 import entity.Arret;
@@ -17,10 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import session.AdministrateurSession;
 
-/**
- *
- * @author Sara
- */
+
 @WebServlet(name = "menu", urlPatterns = {"/menu"})
 public class menu extends HttpServlet {
 
